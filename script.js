85ef555768f4bf3fac0d16c7ae3e794fdae84bf1
michaelmusty <michaelmusty@gmail.com>
2019-09-30 12:58:38 -0400

    travis

---
+++ script.js
@@ -0,0 +1,45 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+
+var light = new THREE.AmbientLight( 0x404040 );
+scene.add(light);
+
+var lights = [];
+lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+lights[ 0 ].position.set( 0, 200, 0 );
+lights[ 1 ].position.set( 100, 200, 100 );
+lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+scene.add( lights[ 0 ] );
+scene.add( lights[ 1 ] );
+scene.add( lights[ 2 ] );
+
+var geometry = new THREE.SphereGeometry(1);
+// var geometry = new THREE.ParametricGeometry((u, v, dest) => {
+//   const x = -5 + u * 10,
+//         y = -5 + v * 10,
+//         z = x * y;
+//         dest.set(x, y, z);
+// }, 32, 32);
+var material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+}
+animate();
