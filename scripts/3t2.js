b52c04a37ffd75f71ad6c4a7ec2b8fc2e1f1d895
michaelmusty <michaelmusty@gmail.com>
2019-10-10 09:17:45 -0400

    reset

--- scripts/3t2.js
+++ scripts/3t2.js
@@ -1,48 +1,85 @@
-var width = 960,
-    height = 500
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-var svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height);
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody().strength(-400))
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
-var force = d3.layout.force()
-    .gravity(.05)
-    .distance(100)
-    .charge(-100)
-    .size([width, height]);
 
-d3.json("data/3t2.json", function(json) {
-  force
-      .nodes(json.nodes)
-      .links(json.links)
-      .start();
+d3.json("data.json", function(error, graph) {
+  if (error) throw error;
+  
+  graph.links.forEach(function(d){
+    d.source = d.source_id;    
+    d.target = d.target_id;
+  });           
 
-  var link = svg.selectAll(".link")
-      .data(json.links)
-    .enter().append("line")
-      .attr("class", "link")
-    .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
+  var link = svg.append("g")
+                .style("stroke", "#aaa")
+                .selectAll("line")
+                .data(graph.links)
+                .enter().append("line");
 
-  var node = svg.selectAll(".node")
-      .data(json.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .call(force.drag);
+  var node = svg.append("g")
+            .attr("class", "nodes")
+  .selectAll("circle")
+            .data(graph.nodes)
+  .enter().append("circle")
+          .attr("r", 6)
+          .call(d3.drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended));
+  
+  var label = svg.append("g")
+      .attr("class", "labels")
+      .selectAll("text")
+      .data(graph.nodes)
+      .enter().append("text")
+        .attr("class", "label")
+        .text(function(d) { return d.name; });
 
-  node.append("circle")
-      .attr("r","5");
+  simulation
+      .nodes(graph.nodes)
+      .on("tick", ticked);
 
-  node.append("text")
-      .attr("dx", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name });
+  simulation.force("link")
+      .links(graph.links);
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-  });
+    node
+         .attr("r", 20)
+         .style("fill", "#d9d9d9")
+         .style("stroke", "#969696")
+         .style("stroke-width", "1px")
+         .attr("cx", function (d) { return d.x+6; })
+         .attr("cy", function(d) { return d.y-6; });
+    
+    label
+    		.attr("x", function(d) { return d.x; })
+            .attr("y", function (d) { return d.y; })
+            .style("font-size", "20px").style("fill", "#4393c3");
+  }
 });
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+  simulation.fix(d);
+}
+
+function dragged(d) {
+  simulation.fix(d, d3.event.x, d3.event.y);
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  simulation.unfix(d);
+}
